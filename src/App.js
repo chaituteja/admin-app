fec0b5b7006ea5c18b320f6c8511c7436d3f722c
chaituteja <j.chaitanyateja@gmail.com>
2025-12-05 10:41:40 +1100

    Created new pages and add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import New from "./pages/new/New";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
