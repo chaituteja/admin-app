a21f9aac87993bee4425a28a05ef3f3496629a78
chaituteja <j.chaitanyateja@gmail.com>
2025-12-05 14:39:34 +1100

    Add charts

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,56 @@
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  { name: "Jan", Total: 1200 },
+  { name: "Feb", Total: 2100 },
+  { name: "Mar", Total: 800 },
+  { name: "Apr", Total: 1260 },
+  { name: "May", Total: 900 },
+  { name: "Jun", Total: 1700 },
+];
+
+export default function Chart({ isAnimationActive = true }) {
+  return (
+    <div className="chart">
+      <div className="title">Last 6 months revenue</div>
+      <AreaChart
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+          maxHeight: "70vh",
+          aspectRatio: 1.618,
+        }}
+        responsive
+        data={data}
+        margin={{ top: 10, right: 0, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis width="auto" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="Total"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#total)"
+          isAnimationActive={isAnimationActive}
+        />
+      </AreaChart>
+    </div>
+  );
+}
---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,48 @@
+import { CircularProgressbar } from "react-circular-progressbar";
+import "./featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+export default function Featured() {
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+        </div>
+        <p className="title">Total Sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">Previous transactions processing</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount ">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpIcon fontSize="small" />
+              <div className="resultAmount ">$22.9k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount ">$52.1k</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
